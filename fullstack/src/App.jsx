32b21c4288a2fec600fdb455a5608835fd8cc0df
IAMOTAS <niiotasalokome@gmail.com>
2025-01-16 11:17:55 +0000

    tailwind-setup

--- fullstack/src/App.jsx
+++ fullstack/src/App.jsx
@@ -3,10 +3,14 @@ import './App.css'
 
 function App() {
   
-
+ 
   return (
     <>
-      
+      <div className='Hero bg-slate-600'>
+
+        <h1>how are you.</h1>
+
+      </div>
       
     </>
   )
